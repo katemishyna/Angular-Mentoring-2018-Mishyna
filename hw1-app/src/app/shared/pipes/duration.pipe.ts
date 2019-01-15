449a250166aeddb1accec1fa5228d254a217ef1f
Kateryna_Mishyna <Kateryna_Mishyna@epam.com>
2019-01-15 13:33:25 +0200

    Minutes fix

--- hw1-app/src/app/shared/pipes/duration.pipe.ts
+++ hw1-app/src/app/shared/pipes/duration.pipe.ts
@@ -5,6 +5,6 @@ export class DurationPipe implements PipeTransform {
   transform(minutes: number): string {
     let hours = (Math.floor(minutes/60));
     let min = minutes - hours*60;
-    return hours > 0 ? `${hours}h ${minutes}min` : `${min}min`;
+    return hours > 0 ? `${hours}h ${min}min` : `${min}min`;
   }
 }
